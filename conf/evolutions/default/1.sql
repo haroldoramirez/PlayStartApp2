74e480597699d1e15abac2f12e94bc4a44616990
Haroldo Ramirez <haroldoramirez@gmail.com>
2016-04-11 14:25:45 -0300

    primeiro commit

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,37 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table token (
+  token                     varchar(255) not null,
+  user_id                   bigint,
+  type                      varchar(8),
+  date_creation             timestamp,
+  email                     varchar(255),
+  constraint ck_token_type check (type in ('password','email')),
+  constraint pk_token primary key (token))
+;
+
+create table usuario (
+  id                        bigserial not null,
+  email                     varchar(255),
+  fullname                  varchar(255),
+  confirmation_token        varchar(255),
+  password_hash             varchar(255),
+  date_creation             timestamp,
+  validated                 boolean,
+  constraint uq_usuario_email unique (email),
+  constraint uq_usuario_fullname unique (fullname),
+  constraint pk_usuario primary key (id))
+;
+
+
+
+
+# --- !Downs
+
+drop table if exists token cascade;
+
+drop table if exists usuario cascade;
+
